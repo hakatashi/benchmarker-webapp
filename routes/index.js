39d3708768d9ec5191f9873df1548d3c044d5668
Koki Takahashi <hakatasiloving@gmail.com>
2016-09-04 05:18:45 +0900

    Create POST /

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
+const {spawn} = require('child_process');
+const concat = require('concat-stream');
+
 const express = require('express');
 const router = express.Router();
+
+let executing = false;
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/', (req, res, next) => {
+  if (executing) {
+    res.json({error: true});
+  } else {
+    const ip = process.env.WEBAPP_IP || '127.0.0.1';
+
+    const benchmarker = spawn('/opt/go/bin/benchmarker', [
+      '-t', `http://${ip}/`,
+      '-u', '/opt/go/src/github.com/catatsuy/private-isu/benchmarker/userdata',
+    ]);
+
+    let result;
+
+    benchmarker.stdout.pipe(concat((data) => {
+      result = JSON.parse(data);
+    }));
+
+    benchmarker.on('close', (code) => {
+      if (code !== 0) {
+        res.json({error: true});
+      } else {
+        res.json({error: false, result});
+      }
+    });
+  }
 });
 
 module.exports = router;
